8dab8ea1fdbd06711eb0658fa9b9715daa356288
daniele-demarchis97 <daniele.demarchis97@gmail.com>
2021-11-17 01:58:44 +0100

    Implement: Home, Header, Navbar, Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { ProjectContainerComponent } from './project/project-container/project-c
 import { ProjectDetailComponent } from './project/project-detail/project-detail.component';
 import { ProjectListComponent } from './project/project-list/project-list.component';
 import { ProjectFormComponent } from './project/project-form/project-form.component';
+import { HomeComponent } from './dashboard/home/home.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { AppRoutingModule } from './app-routing.module';
+import { SectionHeaderComponent } from './shared/section-header/section-header.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +18,15 @@ import { ProjectFormComponent } from './project/project-form/project-form.compon
     ProjectContainerComponent,
     ProjectDetailComponent,
     ProjectListComponent,
-    ProjectFormComponent
+    ProjectFormComponent,
+    HomeComponent,
+    NavbarComponent,
+    SectionHeaderComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
