a0a207ab2af9fe17cfb72da4b5019b072d988703
daniele-demarchis97 <daniele.demarchis97@gmail.com>
2021-11-19 15:53:47 +0100

    Implement: Interceptor, Guard, Form ReactiveDriven

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardModule } from './modules/dashboard/dashboard.module';
+import { AuthInterceptor } from './core/auth.interceptor';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,9 @@ import { DashboardModule } from './modules/dashboard/dashboard.module';
     DashboardModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/modules/project/project.module.ts
+++ src/app/modules/project/project.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { SharedModule } from "@app/shared/shared.module";
 import { ProjectFormComponent } from "./components/project-form/project-form.component";
 import { ProjectListComponent } from "./components/project-list/project-list.component";
@@ -8,6 +8,7 @@ import { ProjectDashboardComponent } from "./pages/project-dashboard/project-das
 import { ProjectDetailComponent } from "./pages/project-detail/project-detail.component";
 import { ProjectRoutingModule } from "./project-routing.module";
 import { ProjectComponent } from './pages/project.component';
+import { ProjectFormRfComponent } from './components/project-form-rf/project-form-rf.component';
 
 @NgModule({
   declarations: [
@@ -15,11 +16,13 @@ import { ProjectComponent } from './pages/project.component';
     ProjectDetailComponent,
     ProjectListComponent,
     ProjectFormComponent,
-    ProjectComponent
+    ProjectComponent,
+    ProjectFormRfComponent
   ],
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     SharedModule,
     ProjectRoutingModule
   ],
